e8434dbc12004b334bac3b7c128a286f0c2ca471
Dave <dave.go@kakaocorp.com>
2017-07-10 15:44:39 +0900

    send only metric of running docker containers

--- module/nvidiadocker/status/status.go
+++ module/nvidiadocker/status/status.go
@@ -3,12 +3,18 @@ package status
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
-
 	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/metricbeat/mb"
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+var (
+	nvidiaDeviceRegexp = regexp.MustCompile("^/dev/nvidia([0-9]+)$")
 )
 
 // init registers the MetricSet with the central registry.
@@ -25,7 +31,12 @@ func init() {
 // multiple fetch calls.
 type MetricSet struct {
 	mb.BaseMetricSet
-	apiURL string
+	apiURL       string
+	dockerClient *docker.Client
+}
+
+type nvidiaStatus struct {
+	Devices []common.MapStr `json:"Devices"`
 }
 
 // New create a new instance of the MetricSet
@@ -34,26 +45,95 @@ type MetricSet struct {
 func New(base mb.BaseMetricSet) (mb.MetricSet, error) {
 
 	config := struct {
-		APIURL string `config:"apiurl"`
+		APIURL         string `config:"apiurl"`
+		DockerEndpoint string `config:"dockerendpoint"`
 	}{
-		APIURL: "",
+		APIURL:         "",
+		DockerEndpoint: "",
 	}
 
 	if err := base.Module().UnpackConfig(&config); err != nil {
 		return nil, err
 	}
 
+	dockerClient, err := docker.NewClient(config.DockerEndpoint)
+	if err != nil {
+		return nil, err
+	}
+
 	return &MetricSet{
 		BaseMetricSet: base,
 		apiURL:        config.APIURL,
+		dockerClient:  dockerClient,
 	}, nil
 }
 
 // Fetch methods implements the data gathering and data conversion to the right format
 // It returns the event which is then forward to the output. In case of an error, a
 // descriptive error must be returned.
-func (m *MetricSet) Fetch() (common.MapStr, error) {
-	resp, err := http.Get(fmt.Sprintf("%s/v1.0/gpu/status/json", m.apiURL))
+// func (m *MetricSet) Fetch() (common.MapStr, error) {
+
+// 	resp, err := http.Get(fmt.Sprintf("%s/v1.0/gpu/status/json", m.apiURL))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	bytes, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer resp.Body.Close()
+
+// 	data := common.MapStr{}
+// 	if err := json.Unmarshal(bytes, &data); err != nil {
+// 		return nil, err
+// 	}
+
+// 	event := common.MapStr{}
+// 	if devices, ok := data["Devices"].([]interface{}); ok {
+// 		for i, device := range devices {
+// 			event.Put(fmt.Sprintf("device%d", i), device)
+// 		}
+// 	}
+
+// 	return event, nil
+// }
+
+// Fetch methods implements the data gathering and data conversion to the right format
+// It returns the event which is then forward to the output. In case of an error, a
+// descriptive error must be returned.
+func (m *MetricSet) Fetch() ([]common.MapStr, error) {
+	apiContainers, err := m.dockerClient.ListContainers(docker.ListContainersOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	if len(apiContainers) == 0 {
+		return []common.MapStr{}, nil
+	}
+
+	gpuDevices, err := getGPUDeviceStatus(m.apiURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return m.fetchFromContainers(apiContainers, gpuDevices)
+}
+
+func (m *MetricSet) fetchFromContainers(apiContainers []docker.APIContainers, gpuDevices []common.MapStr) ([]common.MapStr, error) {
+	events := make([]common.MapStr, 0, len(apiContainers))
+	for _, apiContainer := range apiContainers {
+		if container, err := m.dockerClient.InspectContainer(apiContainer.ID); err == nil {
+			event := fetchFromContainer(container, gpuDevices)
+			if len(event["Devices"].(common.MapStr)) > 0 {
+				events = append(events, event)
+			}
+		}
+	}
+	return events, nil
+}
+
+func getGPUDeviceStatus(apiURL string) ([]common.MapStr, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/v1.0/gpu/status/json", apiURL))
 	if err != nil {
 		return nil, err
 	}
@@ -63,17 +143,30 @@ func (m *MetricSet) Fetch() (common.MapStr, error) {
 	}
 	defer resp.Body.Close()
 
-	data := common.MapStr{}
-	if err := json.Unmarshal(bytes, &data); err != nil {
+	status := nvidiaStatus{}
+	if err := json.Unmarshal(bytes, &status); err != nil {
 		return nil, err
 	}
 
-	event := common.MapStr{}
-	if devices, ok := data["Devices"].([]interface{}); ok {
-		for i, device := range devices {
-			event.Put(fmt.Sprintf("device%d", i), device)
+	return status.Devices, nil
+}
+
+func fetchFromContainer(container *docker.Container, gpuDevices []common.MapStr) common.MapStr {
+	gpuDevicesLen := len(gpuDevices)
+	event := common.MapStr{
+		"ContainerName": container.Name,
+		"Devices":       common.MapStr{},
+	}
+	for _, device := range container.HostConfig.Devices {
+		if findStrs := nvidiaDeviceRegexp.FindStringSubmatch(device.PathOnHost); findStrs != nil && len(findStrs) == 2 {
+			if nvidiaIndex, err := strconv.ParseInt(findStrs[1], 10, 64); err == nil {
+				if int(nvidiaIndex) < gpuDevicesLen {
+					(event["Devices"].(common.MapStr)).Put(fmt.Sprintf("%d", nvidiaIndex), gpuDevices[nvidiaIndex])
+				}
+			}
 		}
 	}
 
-	return event, nil
+	return event
+
 }
---
+++ module/nvidiadocker/status/status_test.go
@@ -0,0 +1,83 @@
+package status
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/elastic/beats/libbeat/common"
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+func TestNvidiaDeviceRegexp(t *testing.T) {
+	testDatas := []struct {
+		DeviceName  string
+		DeviceIndex string
+		Matched     bool
+	}{
+		{
+			"/dev/nvidia0",
+			"0",
+			true,
+		},
+		{
+			"/dev/nvidia12",
+			"12",
+			true,
+		},
+		{
+			"/dev/test0",
+			"",
+			false,
+		},
+		{
+			"/nvidia0",
+			"",
+			false,
+		},
+		{
+			"/dev/nvidia",
+			"",
+			false,
+		},
+	}
+
+	for _, testData := range testDatas {
+		findStrs := nvidiaDeviceRegexp.FindStringSubmatch(testData.DeviceName)
+		if (findStrs != nil) != testData.Matched {
+			t.Fatal("not matched")
+		}
+
+		if testData.Matched {
+			if !reflect.DeepEqual([]string{testData.DeviceName, testData.DeviceIndex}, findStrs) {
+				t.Fatal("not matched")
+			}
+		}
+
+	}
+}
+
+func TestFetchFromContainer(t *testing.T) {
+	devicesJSON := `[{"Power":13,"Temperature":15,"Utilization":{"GPU":0,"Memory":0,"Encoder":0,"Decoder":0},"Memory":{"GlobalUsed":8,"ECCErrors":{"L1Cache":null,"L2Cache":null,"Global":null}},"Clocks":{"Cores":40,"Memory":405},"PCI":{"BAR1Used":2,"Throughput":{"RX":0,"TX":0}},"Processes":null},{"Power":9,"Temperature":14,"Utilization":{"GPU":0,"Memory":0,"Encoder":0,"Decoder":0},"Memory":{"GlobalUsed":7,"ECCErrors":{"L1Cache":null,"L2Cache":null,"Global":null}},"Clocks":{"Cores":40,"Memory":405},"PCI":{"BAR1Used":2,"Throughput":{"RX":0,"TX":0}},"Processes":null},{"Power":9,"Temperature":18,"Utilization":{"GPU":0,"Memory":0,"Encoder":0,"Decoder":0},"Memory":{"GlobalUsed":7,"ECCErrors":{"L1Cache":null,"L2Cache":null,"Global":null}},"Clocks":{"Cores":40,"Memory":405},"PCI":{"BAR1Used":2,"Throughput":{"RX":0,"TX":0}},"Processes":null},{"Power":9,"Temperature":16,"Utilization":{"GPU":0,"Memory":0,"Encoder":0,"Decoder":0},"Memory":{"GlobalUsed":7,"ECCErrors":{"L1Cache":null,"L2Cache":null,"Global":null}},"Clocks":{"Cores":40,"Memory":405},"PCI":{"BAR1Used":2,"Throughput":{"RX":0,"TX":0}},"Processes":null},{"Power":9,"Temperature":20,"Utilization":{"GPU":0,"Memory":0,"Encoder":0,"Decoder":0},"Memory":{"GlobalUsed":7,"ECCErrors":{"L1Cache":null,"L2Cache":null,"Global":null}},"Clocks":{"Cores":40,"Memory":405},"PCI":{"BAR1Used":2,"Throughput":{"RX":0,"TX":0}},"Processes":null},{"Power":9,"Temperature":15,"Utilization":{"GPU":0,"Memory":0,"Encoder":0,"Decoder":0},"Memory":{"GlobalUsed":7,"ECCErrors":{"L1Cache":null,"L2Cache":null,"Global":null}},"Clocks":{"Cores":40,"Memory":405},"PCI":{"BAR1Used":2,"Throughput":{"RX":0,"TX":0}},"Processes":null},{"Power":9,"Temperature":18,"Utilization":{"GPU":0,"Memory":0,"Encoder":0,"Decoder":0},"Memory":{"GlobalUsed":7,"ECCErrors":{"L1Cache":null,"L2Cache":null,"Global":null}},"Clocks":{"Cores":40,"Memory":405},"PCI":{"BAR1Used":2,"Throughput":{"RX":0,"TX":0}},"Processes":null},{"Power":9,"Temperature":17,"Utilization":{"GPU":0,"Memory":0,"Encoder":0,"Decoder":0},"Memory":{"GlobalUsed":7,"ECCErrors":{"L1Cache":null,"L2Cache":null,"Global":null}},"Clocks":{"Cores":40,"Memory":405},"PCI":{"BAR1Used":2,"Throughput":{"RX":0,"TX":0}},"Processes":null}]`
+	gpuDevices := []common.MapStr{}
+	if err := json.Unmarshal([]byte(devicesJSON), &gpuDevices); err != nil {
+		t.Fatal(err)
+	}
+
+	event := fetchFromContainer(&docker.Container{
+		ID:   "id1",
+		Name: "name1",
+		HostConfig: &docker.HostConfig{
+			Devices: []docker.Device{
+				{
+					PathOnHost:      "/dev/nvidia0",
+					PathInContainer: "/dev/nvidia0",
+				},
+			},
+		},
+	}, gpuDevices)
+
+	if _, err := (event["Devices"].(common.MapStr)).GetValue("0"); err != nil {
+		t.Fatal(err)
+	}
+}
